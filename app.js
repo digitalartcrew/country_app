8b4a777dcf22943a83db3bcf491a54fca1629d47
donte <digitalartcrew@gmail.com>
2015-09-22 14:13:57 -0700

    Updated routes

--- app.js
+++ app.js
@@ -3,21 +3,65 @@ var express = require('express'),
   app = express(),
   bodyParser = require("body-parser"),
   methodOverride = require('method-override'),
+  session = require("cookie-session"),
   morgan = require('morgan');
 db = require("./models");
+var loginMiddleware = require("./middleware/loginHelper");
+var routeMiddleware = require("./middleware/routeHelper");
 
 app.set('view engine', 'ejs');
 app.use(morgan('combined'));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: true})); 
 app.use(methodOverride('_method'));
+app.use(session({
+  maxAge: 3600000,
+  secret: 'illnevertell',
+  name: "chocolate chip"
+}));
+
+// use loginMiddleware everywhere!
+app.use(loginMiddleware);
+
+app.get('/', routeMiddleware.ensureLoggedIn, function(req,res){
+  res.render('users/index');
+});
+
+app.get('/signup', routeMiddleware.preventLoginSignup ,function(req,res){
+  res.render('users/signup');
+});
+
+app.post("/signup", function (req, res) {
+  var newUser = req.body.user;
+  db.User.create(newUser, function (err, user) {
+    if (user) {
+      req.login(user);
+      res.redirect("/countries");
+    } else {
+      console.log(err);
+      // TODO - handle errors in ejs!
+      res.render("users/signup");
+    }
+  });
+});
 
 
-app.set('view engine', 'ejs');
-app.use(morgan('combined'));
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({extended: true})); 
-app.use(methodOverride('_method'));
+app.get("/login", routeMiddleware.preventLoginSignup, function (req, res) {
+  res.render("users/login");
+});
+
+app.post("/login", function (req, res) {
+  db.User.authenticate(req.body.user,
+  function (err, user) {
+    if (!err && user !== null) {
+      req.login(user);
+      res.redirect("/countries");
+    } else {
+      // TODO - handle errors in ejs!
+      res.render("users/login");
+    }
+  });
+});
 
 
 app.get('/', function(req,res){
@@ -25,7 +69,7 @@ app.get('/', function(req,res){
 });
 
 // INDEX
-app.get('/countries', function(req,res){
+app.get('/countries', routeMiddleware.ensureLoggedIn, function(req,res){
   db.Country.find({},function(err,countries){
     if (err) throw err;
     res.render("countries/index", {countries:countries});
@@ -33,12 +77,12 @@ app.get('/countries', function(req,res){
 });
 
 // NEW
-app.get('/countries/new', function(req,res){
+app.get('/countries/new', routeMiddleware.ensureLoggedIn, function(req,res){
   res.render("countries/new");
 });
 
 // CREATE
-app.post('/countries', function(req,res){
+app.post('/countries', routeMiddleware.ensureLoggedIn, function(req,res){
     var country = new db.Country(req.body.country);
     var cities = req.body.cities.split(", ");
     country.cities = cities;
@@ -50,7 +94,7 @@ app.post('/countries', function(req,res){
 
 // SHOW
 
-app.get('/countries/:id', function(req,res){
+app.get('/countries/:id', routeMiddleware.ensureLoggedIn, function(req,res){
   db.Country.findById(req.params.id,function(err,country){
     if (err) throw err;
     res.render("countries/show", {country:country});
@@ -59,7 +103,7 @@ app.get('/countries/:id', function(req,res){
 
 // EDIT
 
-app.get('/countries/:id/edit', function(req,res){
+app.get('/countries/:id/edit', routeMiddleware.ensureLoggedIn, function(req,res){
   db.Country.findById(req.params.id,function(err,country){
     if (err) throw err;
     res.render("countries/edit", {country:country});
@@ -67,7 +111,7 @@ app.get('/countries/:id/edit', function(req,res){
 });
 
 // UPDATE
-app.put('/countries/:id', function(req,res){
+app.put('/countries/:id', routeMiddleware.ensureLoggedIn, function(req,res){
   db.Country.findById(req.params.id,function(err,country){
     // loop over all keys in object
     for(var prop in req.body.country){
@@ -82,7 +126,7 @@ app.put('/countries/:id', function(req,res){
 });
 
 // DESTROY
-app.delete('/countries/:id', function(req,res){
+app.delete('/countries/:id', routeMiddleware.ensureLoggedIn, function(req,res){
     db.Country.findByIdAndRemove(req.params.id, function(err,book){
       if (err) throw err;
       res.redirect('/');
--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/country_app");
 
-
+module.exports.User = require("./user");
 module.exports.Country = require("./country");
